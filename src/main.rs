f978704f85140e46b3ef5be69ef8eb22c8625278
Viraj Shah <veer15102003@gmail.com>
2023-09-08 11:05:23 +0530

    added main function

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn testing(requesr: HttpRequest) -> impl Responder {
+    let name = requesr.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(testing))
+            .route("/{name}", web::get().to(testing))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
