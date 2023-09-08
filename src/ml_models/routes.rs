41910c5c89eac50c9b037baacf2fe60dafcaf654
Viraj Shah <veer15102003@gmail.com>
2023-09-08 13:21:58 +0530

    added crud routes

--- src/ml_models/routes.rs
+++ src/ml_models/routes.rs
@@ -1,8 +1,10 @@
+use crate::error_handler::CustomError;
 use crate::ml_models::Model;
+use crate::ml_models::Models;
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 use serde_json;
 
-#[get("/models")]
+#[get("/dummy_models")]
 async fn dummy_get_all() -> impl Responder {
     HttpResponse::Ok().json(vec![
         Model {
@@ -26,7 +28,7 @@ async fn dummy_get_all() -> impl Responder {
     ])
 }
 
-#[get("/models/{id}")]
+#[get("/dummy_models/{id}")]
 async fn dummy_get_by_index() -> impl Responder {
     HttpResponse::Ok().json(Model {
         id: 123,
@@ -39,7 +41,45 @@ async fn dummy_get_by_index() -> impl Responder {
     })
 }
 
+#[get("/models")]
+async fn get_all() -> Result<HttpResponse, CustomError> {
+    let employees = Models::find_all()?;
+    Ok(HttpResponse::Ok().json(employees))
+}
+
+#[get("/model/{id}")]
+async fn get_model_by_id(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let employee = Models::find(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[post("/models")]
+async fn create(model: web::Json<Model>) -> Result<HttpResponse, CustomError> {
+    let employee = Models::create(model.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[put("/models/{id}")]
+async fn update(
+    id: web::Path<i32>,
+    employee: web::Json<Models>,
+) -> Result<HttpResponse, CustomError> {
+    let employee = Models::update(id.into_inner(), employee.into_inner())?;
+    Ok(HttpResponse::Ok().json(employee))
+}
+
+#[delete("/models/{id}")]
+async fn delete(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let deleted_employee = Models::delete(id.into_inner())?;
+    Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_employee })))
+}
+
 pub fn init_routes(config: &mut web::ServiceConfig) {
     config.service(dummy_get_all);
     config.service(dummy_get_by_index);
+    config.service(get_all);
+    config.service(get_model_by_id);
+    config.service(create);
+    config.service(update);
+    config.service(delete);
 }
